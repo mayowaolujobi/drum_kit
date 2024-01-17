24cc9f00a70b3301aafc7fff1277ac6a6140b967
mayowaolujobi <51064480+mayowaolujobi@users.noreply.github.com>
2024-01-17 17:12:07 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,91 @@
+/*
+//Test 1 
+
+//document.querySelector("button").addEventListener("click", buttonClick);
+
+function buttonClick(){
+  alert("I got clicked");
+}
+*/
+
+
+
+/*
+//Test for audio.play();
+
+numberOfDrumButtons = document.querySelectorAll(".drum").length;  // number of drum buttons
+
+//for loop containing eventlistener for drum buttons with anonymous function
+for(i=0; i < numberOfDrumButtons; i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+
+    const audio = new Audio('sounds/tom-1.mp3');
+    audio.play();
+
+  });
+}
+
+*/
+
+
+
+
+numberOfDrumButtons = document.querySelectorAll(".drum").length;  // number of drum buttons
+
+//for loop containing eventlistener for drum buttons with anonymous function
+for(i=0; i < numberOfDrumButtons; i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+
+
+    var buttonInnerHTML = this.innerHTML;
+    
+    switch(buttonInnerHTML){
+     case "w":
+        let tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+
+     case "a":
+        let tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+
+     case "s":
+         let tom3 = new Audio("sounds/tom-3.mp3");
+         tom3.play();
+         break;
+
+
+     case "d":
+         let tom4 = new Audio("sounds/tom-4.mp3");
+         tom4.play();
+         break;
+
+
+     case "j":
+         let snare = new Audio("sounds/snare.mp3");
+         snare.play();
+         break;
+
+
+     case "k":
+         let crash = new Audio("sounds/crash.mp3");
+         crash.play();
+         break;
+
+
+     case "l":
+         let kick = new Audio("sounds/kick-bass.mp3");
+         kick.play();
+         break;  
+
+
+        default: console.log(buttonInnerHTMLinnerHTML);
+    }
+
+
+
+
+  });
+}
+
